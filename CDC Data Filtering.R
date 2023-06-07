6969d619db7654f303dd33aece25e9ad02ae0b7e
HerambGupta <gupta.heramb@gmail.com>
2023-06-07 17:06:57 -0400

    Update

---
+++ CDC Data Filtering.R
@@ -0,0 +1,33 @@
+library(dplyr)
+library(tidyverse)
+library(lubridate)
+library(ggplot2)
+final<-data.frame()
+
+for(i in c("COOK","BRONX","SAN DIEGO","MIAMI-DADE")){
+cdc_data<-read.csv(paste(i,".csv",sep=""))
+
+cdc_data<-cdc_data %>%
+  mutate(year=year(ymd(paste(case_month,"-01"))),
+         month=month(ymd(paste(case_month,"-01"))))%>%
+  select(c("year","month"))
+
+cdc_month<- cdc_data %>%
+  group_by(year,month)%>%
+  summarise(case=n()) %>%
+  mutate(county=i)
+
+final<-rbind(final,cdc_month)
+}
+final<-final%>%
+  mutate(date=as.Date(paste(year,month,1,sep="-"),format="%Y-%m-%d"))
+
+write.csv(final,file="month_wise_cases_per_county.csv")
+
+final%>%
+  ggplot()+
+  geom_col(aes(x=date,y=case))+
+  facet_wrap(~county)
+
+
+  
