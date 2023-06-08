d6256a67f0121f1bf4a9784a864fd6f7086d44e2
HerambGupta <gupta.heramb@gmail.com>
2023-06-08 00:16:57 -0400

    update

--- CDC Data Filtering.R
+++ CDC Data Filtering.R
@@ -10,6 +10,8 @@ cdc_data<-read.csv(paste(i,".csv",sep=""))
 cdc_data<-cdc_data %>%
   mutate(year=year(ymd(paste(case_month,"-01"))),
          month=month(ymd(paste(case_month,"-01"))))%>%
+  filter(ethnicity=="Hispanic/Latino",
+         age_group %in% c("50 to 64 years","18 to 49 years","65+ years")) %>%
   select(c("year","month"))
 
 cdc_month<- cdc_data %>%
@@ -22,12 +24,12 @@ final<-rbind(final,cdc_month)
 final<-final%>%
   mutate(date=as.Date(paste(year,month,1,sep="-"),format="%Y-%m-%d"))
 
-write.csv(final,file="month_wise_cases_per_county.csv")
+write.csv(final,file="month_wise_cases_per_county_exclusive.csv")
 
 final%>%
   ggplot()+
   geom_col(aes(x=date,y=case))+
-  facet_wrap(~county)
+  facet_wrap(~county,scale="free_y")
 
 
   
